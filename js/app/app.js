481477960fed558b63ad252f4f86fb995c3d8784
Erik-Brgs <erik.borges@ufpr.br>
2021-04-28 15:53:16 -0300

    final commit

--- js/app/app.js
+++ js/app/app.js
@@ -1,1 +1,12 @@
-angular.module("meuModulo",[])
+angular.module("meuModulo",['ngRoute'])
+
+.config(function($routeProvider){
+	$routeProvider
+	.when("/home",{
+		templateUrl:"templates/home.html",
+		controller:"indexController"
+	})
+
+	$routeProvider.otherwise({redirectTo:"/home"});
+
+})
